6ed13b96c8eb78bdc304c338280f8360b374f1a7
vanessa <vipyne@gmail.com>
2014-02-09 02:46:57 -0500

    well on our way to parsing mad json

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,23 @@ class PagesController < ApplicationController
   end
 
   def new
+    @survey = Survey.new
     @survey_questions = get_json
   end
 
   def create
+    @survey = Survey.new survey_params
+    if @survey.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def survey_params
+    params.require(:survey).permit(:json_input, :json_output)
   end
 
 end
